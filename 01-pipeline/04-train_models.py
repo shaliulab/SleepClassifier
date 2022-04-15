0000622e89bf81e441185d5fcc4f26152134c90e
antortjim <ntoniohu@gmail.com>
2022-04-15 14:08:24 +0000

    merge pipelines and update config.yaml

---
+++ 01-pipeline/04-train_models.py
@@ -0,0 +1,44 @@
+# train-torch-model y --output results/KC-models/500_neurons_NN/ --seed 1000 --model-name NeuralNetwork --h5ad-input results/KC-data/KC-raw.h5ad --training-config training_params.yaml --label-mapping data/templates/simple_condition_mapping.yaml
+
+#
+import yaml
+import pandas as pd
+import os.path
+
+from sleep_models.bin.train_model import train
+
+with open("config.yaml", "r") as filehandle:
+    config = yaml.load(filehandle, yaml.SafeLoader)
+
+DATA_DIR = config["data_dir"]
+RESULTS_DIR = config["results_dir"]
+CONDITION_TEMPLATE = config["template"]
+
+if CONDITION_TEMPLATE is None:
+    label_mapping=None
+else:
+    label_mapping=os.path.join(DATA_DIR, "templates", CONDITION_TEMPLATE)
+
+def get_celltypes(background):
+
+    return pd.read_csv(
+        os.path.join(
+            DATA_DIR, "backgrounds", f"{background}.csv"
+        )
+    )["cluster"].tolist()
+
+
+for background in config["background"]:
+    for arch in config["arch"]:
+        for seed in [1000, 2000, 3000]:
+            celltypes = get_celltypes(background)
+            for celltype in celltypes:
+                train(
+                    h5ad_input=os.path.join(DATA_DIR, "h5ad", f"{background}-no-marker-genes.h5ad"),
+                    arch=arch,
+                    cluster=celltype,
+                    output=os.path.join(RESULTS_DIR, f"{background}-train"),
+                    random_state=seed,
+                    highly_variable_genes=config["highly_variable_genes"],
+                    label_mapping=label_mapping,
+                )
--- 01-pipeline/README.md
+++ 01-pipeline/README.md
@@ -3,4 +3,6 @@ and have it predict it on the same and all other celltypes
 Produces a matrixplot that summarises the accuracy of the model depending on which cell type's data was used to train it,
 and which cell type's data was used to predict.
 A diagonal is expected to arise if the sleep signature has some cell specific component.
- 
+ Pipeline to train a model on different cell types, learning the transcriptomic signature of different sleep drive conditions, or any grouping of the experimental conditions of Dopp. et al.
+
+Based on intermediate results of `01-pipeline_sleep-vs-wake`
